16e713895e5493e6c1e99292e330933ece32f5c2
william marques <89227491+kogmau@users.noreply.github.com>
2022-02-17 18:02:39 -0300

    bloco 2 springboot
    primeira aplicação spring

---
+++ exercicios springboot/mentalidade.zip_expanded/mentalidade/src/main/java/com/exercicios/mentalidade/MentalidadeApplication.java
@@ -0,0 +1,13 @@
+package com.exercicios.mentalidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalidadeApplication.class, args);
+	}
+
+}
